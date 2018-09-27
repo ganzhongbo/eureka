7766c6b0c39152fbee203a2ab2bf4bd7a166f727
ganpo <gan.po@cpclub.cn>
2018-09-27 16:49:23 +0800

    update

---
+++ src/main/java/cn/vpclub/wh/EurekaApplication.java
@@ -0,0 +1,18 @@
+package cn.vpclub.wh;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 泥水佬
+ * @date 2017/12/15
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
